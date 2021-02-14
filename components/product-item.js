e07973acb75a64393d817f2c7ff4e8f9b97a4afb
cmwall0605 <cmwall@ucsd.edu>
2021-02-13 20:13:38 -1000

    Added comments to the js

--- components/product-item.js
+++ components/product-item.js
@@ -1,16 +1,25 @@
 // product-item.js
 
 
+/**
+ * Class which contains the functionality of the product-item HTML element.
+ */
 class ProductItem extends HTMLElement {
 
+  // Used to determine if the item is or is not in the cart. Important for
+  //   determining if the item was found in the local storage.
   inCart = false;
 
+  /**
+   * Constructor of the product-item which sets up the style, li, img, price,
+   *   button, and title elements.
+   */
   constructor() {
 
     super();
 
+    // Styling element; copied that styling from the style.css file
     let style = document.createElement('style');
-
     style.textContent = `.price {
       color: green;
       font-size: 1.8em;
@@ -75,83 +84,116 @@ class ProductItem extends HTMLElement {
       text-overflow: unset;
     }`
 
+    // Creates a shadow root
     this.root = this.attachShadow({mode: 'open'});
 
+    // The li element with the class product; every other element is housed in 
+    //   the li.
     let product = document.createElement('li');
     product.setAttribute('class', 'product');
 
+    // Image of the product (img).
     const img = product.appendChild(document.createElement('img'));
 
+    // Title of the product (p).
     const title = product.appendChild(document.createElement('p'));
     title.setAttribute('class', 'title');
 
+    // Price of the product (p).
     const price = product.appendChild(document.createElement('p'));
     price.setAttribute('class', 'price');
 
+    // Button which triggers buttonClick on click.
     const button = product.appendChild(document.createElement('button'));
     button.onclick = () => this.buttonClick(button, title);
     button.innerText = 'Add to Cart';
-
-    const linkElem = document.createElement('link');
-    linkElem.setAttribute('rel', 'stylesheet');
-    linkElem.setAttribute('href', 'style.css');
 
     this.root.append(style);
     this.root.append(product);
   }
 
+  /**
+   * Attributes of the product-item. Inclues the img src, the title, and the
+   *   price.
+   */
   static get observedAttributes() {
     return ['title-text', 'price-text', 'img'];
   }
 
+  /**
+   * 
+   * @param {*} button Reference of the button of the product-item
+   * @param {*} title Reference of the p element housing the title of the
+   *                  product-item
+   */
   buttonClick(button, title) {
 
     const myStorage = window.localStorage;
 
     if(!this.inCart){
 
+      // Increment the cart count by 1.
       document.getElementById('cart-count').innerHTML = 
         parseInt(document.getElementById('cart-count').innerHTML) + 1;
 
       button.innerText = 'Remove from Cart';
 
+      // Add the item into local storage so it can be remembered if it was in
+      //   the cart after refreshing.
       myStorage.setItem(title.innerHTML, 'in cart');
 
       alert('Added to Cart!');
 
     } else {
 
+      // Decrement the cart count by 1.
       document.getElementById('cart-count').innerHTML = 
         parseInt(document.getElementById('cart-count').innerHTML) - 1;
 
       button.innerText = 'Add to Cart';
 
+      // Remoe the item from the local storage as it is no longer in the cart.
       myStorage.removeItem(title.innerHTML);
     }
 
     this.inCart = !this.inCart;
   }
 
+  /**
+   * Called whenever one of the observed attributes is changed.
+   * @param {*} name name of the attribute
+   * @param {*} oldValue the old value of the attribute (not used)
+   * @param {*} newValue the new value of the attribute.
+   */
   attributeChangedCallback(name, oldValue, newValue) {
 
+    // Get the li which houses all the elements which could pssibly be changed
+    //   by the attributes.
     const product = this.shadowRoot.lastChild;
 
     if(name == 'title-text') {
 
+      // Search for the title and image children in the product li element.
       for(let i = 0; i < product.childNodes.length; i++) {
 
+        // Set the inner text of the p title as the new value.
         if(product.childNodes[i].className == 'title') {
 
           product.childNodes[i].innerText = newValue;
 
-          return;
+        // Set the alt text of the image as the new value.
+        } else if(product.childNodes[i].nodeName == 'IMG') {
+
+          product.childNodes[i].alt = newValue;
         }
       }
 
     } else if (name == 'price-text') {
 
+      // Search for the price child in the product li element.
       for(let i = 0; i < product.childNodes.length; i++) {
 
+        // Set the inner text of the p price as the new value.
         if(product.childNodes[i].className == 'price') {
 
           product.childNodes[i].innerText = newValue;
@@ -162,8 +204,10 @@ class ProductItem extends HTMLElement {
 
     } else if (name == 'img') {
 
+      // Search for the image child in the product li element.
       for(let i = 0; i < product.childNodes.length; i++) {
         
+        // Set the source of the image as the new value.
         if(product.childNodes[i].nodeName == 'IMG') {
 
           product.childNodes[i].src = newValue;
@@ -174,6 +218,12 @@ class ProductItem extends HTMLElement {
     }
   }
 
+  /**
+   * Called in the script after being created. Determines if the product was in
+   *   the cart by looking at the local storage, and if it was, change the
+   *   button to reflect this.
+   * @returns If it is in the cart.
+   */
   isInCart () {
 
     const myStorage = window.localStorage;
@@ -184,6 +234,7 @@ class ProductItem extends HTMLElement {
 
     let title;
 
+    // Get the button and title children in the product element.
     for(let i = 0; i < product.childNodes.length; i++) {
 
       if(product.childNodes[i].nodeName == 'BUTTON') {
@@ -196,6 +247,8 @@ class ProductItem extends HTMLElement {
       }
     }
 
+    // If the storage finds the key with the item's title, set up the button
+    //   to reflect this, set inCart to true and return true.
     if(myStorage.getItem(title.innerHTML) != null) {
 
       button.innerText = 'Remove from Cart';
--- scripts/script.js
+++ scripts/script.js
@@ -2,24 +2,40 @@
 
 const myStorage = window.localStorage;
 
+// When the DOM content is loaded, do the following:
 window.addEventListener('DOMContentLoaded', () => {
 
+  // If the local storage is empty, this means it is first time the
+  //   user entered, meaning they should fetch the products.
   if(myStorage.length == 0) {
+
+    // Fetch the products, then store them into local storage, and finally
+    //   load the products onto the store page.
     fetch('https://fakestoreapi.com/products')
       .then(response => response.json())
       .then(data => storeProducts(data))
       .then(() => loadProducts());
+
   } else {
+
+    // Otherwise simply load the products.
     loadProducts();
   }
 });
 
+/**
+ * Stores the given products into local storage.
+ * @param {*} products Array of the product information fetched.
+ */
 function storeProducts(products) {
 
   let i;
 
+  // Set the product count to be equal to the amount of products given.
   myStorage.setItem("productCount", products.length);
 
+  // For each product, insert the title, price, and image of the product into
+  //   local storage.
   for(i = 0; i < products.length; i++) {
 
     myStorage.setItem(products[i].id + "-title", products[i].title);
@@ -28,30 +44,41 @@ function storeProducts(products) {
   }
 }
 
+/**
+ * Load the products from local storage onto the store page.
+ */
 function loadProducts() {
 
   let i;
 
   let ul = document.getElementById("product-list")
 
+  // For each product, load it into the store page
   for(i = 1; i <= myStorage.getItem("productCount"); i++) {
 
+    // Get the title, price, and image of the current product from the local
+    //   storage.
     let title = myStorage.getItem(i + "-title");
     let price = myStorage.getItem(i + "-price");
     let img = myStorage.getItem(i + "-image");
 
+    // Create the product-item element
     const product = document.createElement('product-item');
 
+    // Insert the attributes of the product-item.
+    product.setAttribute('img', img);
     product.setAttribute('title-text', title);
     product.setAttribute('price-text', price);
-    product.setAttribute('img', img);
 
+    // If the product was in the cart from a previous session, then increment
+    // the cart to show this.
     if(product.isInCart()) {
 
       document.getElementById('cart-count').innerHTML = 
         parseInt(document.getElementById('cart-count').innerHTML) + 1;
     }
 
+    // Finally, append the product into the list
     ul.appendChild(product);
 
   }
